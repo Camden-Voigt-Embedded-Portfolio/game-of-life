9b12868682075d84f1411632b5a92bd30d3d5c00
Camden Voigt <camdenvoigt@gmail.com>
2026-01-31 13:16:34 -0800

    Game of life runs on lights

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,34 @@
 #![no_main]
 #![no_std]
 
+mod life;
+use life::*;
+
+use microbit::hal;
 use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
 use cortex_m_rt::entry;
 use microbit::board::Board;
+use microbit::display::blocking::Display;
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let _board = Board::take().unwrap();
-    let mut counter = 0u64;
+    let mut display = Display::new(_board.display_pins);
+    let mut timer = hal::Timer::new(_board.TIMER0);
+    let mut life_grid = [
+        [0, 1, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 0],
+        [0, 1, 0, 0, 0],
+    ];
+
     loop {
-        rprintln!("{}", counter);
-        counter += 1;
+        display.show(&mut timer, life_grid, 1000);
+        life(&mut life_grid);
+        core::hint::spin_loop();
     }
 }
